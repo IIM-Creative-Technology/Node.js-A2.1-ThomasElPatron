a63ae883d2420a6775c975c524257e4c60f0597f
Thomas Fi <tom.filhol@gmail.com>
2023-01-28 14:30:56 +0100

    :sparkles: chat fonctionnel

---
+++ index.js
@@ -0,0 +1,23 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`)
+})
+
+io.on('connection', (socket) =>{
+    console.log('un utilisateur c connecté')
+
+    socket.on('disconnect', () => {
+        console.log('user deconnecté')
+    })
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    })
+})
+
+server.listen(3000, () => {
+    console.log('Port 3000 actif')
+})
