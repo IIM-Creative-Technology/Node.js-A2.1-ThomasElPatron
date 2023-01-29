27e220a5a5fa1af497de00d3c267067b95a795d7
chevalierblu00 <113120254+chevalierblu00@users.noreply.github.com>
2023-01-30 00:06:59 +0100

    login

---
+++ README.md
@@ -0,0 +1,11 @@
+TEST1:
+
+Chat fonctionnel avec l'initiation du serveur/les routes et l'index sur le même serveur:<br>
+  -Http<br>
+  -Express<br>
+  -Socket.io<br>
+  -Connection à mongoDB<br>
+  -Insertion du chat dans MONGODB<br>
+  
+  Thomas Filhol
+  
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://thomas:Lemdp123@thomaselpatron.bopfink.mongodb.net/test";
@@ -10,53 +12,64 @@ app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/public/index.html`)
 })
 
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/public/login.html`)
+})
+
 io.on('connection', (socket) =>{
-    console.log('un utilisateur c connecté')
+console.log('un utilisateur c connecté')
 
-    socket.on('disconnect', () => {
-        console.log('user deconnecté')
-    })
+socket.on('disconnect', () => {
+    console.log('user deconnecté')
+})
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    })
+socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+})
 })
 
 app.get('/', (req, res) => {
-    res.send('Page d\'accueil');
+    res.send("Page d'accueil");
 });
 
 app.get('/register', (req, res) => res.sendFile(__dirname + '/public/register.html'))
 
 app.get('/login', (req, res) => res.sendFile(__dirname + '/public/login.html'))
 
-
-
-
 const userSchema = new mongoose.Schema({
-    username: String,
-    email: String,
-    password: String
+username: String,
+email: String,
+password: String
 });
 const User = mongoose.model('User', userSchema);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/register', (req, res) => {
-    const user = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    });
-    user.save((err) => {
-        if (err) {
-            console.log("finito")
-        } else {
-            // rediriger vers une page de confirmation
-            res.redirect('/');
-        }
-    });
+const user = new User({
+username: req.body.username,
+email: req.body.email,
+password: req.body.password
+});
+user.save((err) => {
+if (err) {
+console.log("finito")
+} else {
+// rediriger vers une page de confirmation
+res.redirect('/');
+}
+});
+
+if (req.method === 'POST' && req.url === '/login') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Hello World!' }));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found');
+  }
+
+
 });
 
 server.listen(3000, () => {
-    console.log('Port 3000 actif')
+console.log('Port 3000 actif')
 })
